93f511477b359d60f43c47a1f83a64ce43388188
Oscar Alvarez <gavinalv6@gmail.com>
2021-04-15 16:45:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,10 +19,31 @@ class Song
   end
 
   def self.genres
-    @@genres
+    @@genres.uniq
   end
 
+  def self.genre_count
+    denre count = {}
+    @@genres.each do |genre|
+      if genre_count|genre|
+        genre_count[genre] += 1
+      else 
+        genre_count[genre] = 1
+      end 
+    end 
   def self.artists
-    @@artists
+    @@artists.uniq
   end
+
+  def self.artist_count
+    artist_count = {}
+      @@artist.each do |artist|
+        if artist_count[artist]
+          artist_count[artist] +=1
+        else artist_count[artist] = 1
+        end
+      end
+      artist_count
+    end
+    
 end
