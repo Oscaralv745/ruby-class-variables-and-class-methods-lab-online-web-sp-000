74ad072ec68415297e244370a680f8552d7b972f
Oscar Alvarez <gavinalv6@gmail.com>
2021-04-15 16:26:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,15 @@
 class Song
-  @@count = 0 
+  @@count = 0
+
   attr_accessor :name, :artist, :genre
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
 
+  def self.count
+    @@count
+  end
 end
